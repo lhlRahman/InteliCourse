8f6f43858d451cedcfab675ffcf7792b40f7e2a0
Abdul <lhlrahman@gmail.com>
2024-03-31 07:43:25 -0400

    navbat

--- components/AlertWrapper/index.js
+++ components/AlertWrapper/index.js
@@ -2,7 +2,7 @@
 import { useEffect } from "react";
 import styles from "../../styles/AlertWrapper.module.scss";
 import ModalText from "../ModalText";
-import { useData } from "@/context/DataContext";
+import { useData } from "../../context/DataContext";
 
 export default function AlertWrapper() {
   const { alerts, setAlerts } = useData();
