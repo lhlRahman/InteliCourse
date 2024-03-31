fd189d30806d705c53699eff8324b163c52f4295
Abdul <lhlrahman@gmail.com>
2024-03-31 07:03:21 -0400

    Update import paths in UI components

--- app/not-found.jsx
+++ app/not-found.jsx
@@ -1,51 +1,14 @@
 
 // app/page.js
 "use client";
-import { useState } from "react";
-import { createWorker } from "tesseract.js";
-import Chat from "@/components/Chat";
+import ChatPopup from "../components/ChatPopup";
 export default function Component() {
-  const [fileName, setFileName] = useState('');
-  const [textResult, setTextResult] = useState('Nothing yet');
-
-  const handleFileChange = async (event) => {
-    const file = event.target.files[0];
-    if (file) {
-      setFileName(file.name);
-
-      // Initialize a Tesseract Worker
-      const worker = createWorker({
-        logger: (m) => console.log(m), // Log progress
-        // Example of setting a configuration option:
-        // Adjust according to your specific requirements.
-    
-      });
-
-      try {
-        await worker.load();
-        await worker.loadLanguage('eng'); // Specify the language
-        await worker.initialize('eng');
-        await worker.setParameters({
-          tessedit_char_whitelist: ' ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789', // Example: Only recognize alphanumeric characters
-        });
-
-        // Recognize the text from the image file
-        const { data: { text } } = await worker.recognize(file);
-
-        setTextResult(text);
-
-        // Terminate the worker after finishing
-        await worker.terminate();
-      } catch (error) {
-        console.error('OCR Error:', error);
-        setTextResult('Failed to recognize text');
-      }
-    }
-  };
 
   return (
-    <div>
-      <Chat></Chat>
+    <div className="h-screen w-screen">
+      <div className="w-1/6">
+      <ChatPopup/>
+      </div>
     </div>
   );
 }
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,7 @@
 "use client";
 import React, { useState } from "react";
 import { HoveredLink, Menu, MenuItem, ProductItem } from "./ui/navbar-menu";
-import { cn } from "@/utils/cn";
+import { cn } from "../utils/cn";
 import Link from "next/link";
 
 export default function Navbar({ className }: { className?: string }) {
