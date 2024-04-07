6e3e954d2531744ad84fe3714afc5b6ce900d073
Abdul <lhlrahman@gmail.com>
2024-04-07 17:29:20 -0400

    Fix bug in login functionality

--- components/chatPopup.jsx
+++ components/chatPopup.jsx
@@ -1,11 +1,11 @@
-"use client";
-import React, { useState } from "react";
+'use client';
+import React, { useState } from 'react';
 import Chat from "./Chat";
 import { CiChat1 } from "react-icons/ci";
 
-const ChatPopup = ({ summary, videoQuery }) => {
-  const [opened, setOpened] = useState(false);
-  const prompt = `You are an upbeat, encouraging tutor who helps students understand ${videoQuery} by explaining ideas and asking students questions. Start by introducing yourself to 
+const ChatPopup = ({summary, videoQuery}) => {
+    const [opened, setOpened] = useState(false);
+    const prompt = `You are an upbeat, encouraging tutor who helps students understand ${videoQuery} by explaining ideas and asking students questions. Start by introducing yourself to 
     the student as their AI-Tutor who is happy to help them with any questions, here is a summary of the video the student has watched ${summary}. Only ask one question at a time. First, ask them what they would like to learn about. Wait for the response. 
     Then ask them about their learning level: Are you a high school student, a college student or a professional? Wait for their response. Then ask them what they know already about the topic they have chosen. Wait for a response. 
     Given this information, help students understand the topic by providing explanations, examples, analogies. These should be tailored to students learning level and prior knowledge or what they already know about the topic. Give students explanations, 
@@ -16,23 +16,23 @@ const ChatPopup = ({ summary, videoQuery }) => {
       hen a student demonstrates that they know the concept you can move the conversation to a close and tell them you’re here to help if they have further questions.
     `;
 
-  const chatStyle = opened
-    ? "fixed bottom-16 right-0 m-4 transition-all duration-3000 ease-in-out transform"
-    : "transition-all duration-500 ease-in-out transform hidden";
+    const chatStyle = opened
+        ? 'fixed bottom-16 right-0 m-4 transition-all duration-3000 ease-in-out transform'
+        : 'transition-all duration-500 ease-in-out transform hidden';
 
-  return (
-    <div className="fixed bottom-0 right-0 m-4">
-      <div className={chatStyle}>
-        <Chat prompt={prompt} />
-      </div>
-      <button
-        onClick={() => setOpened(!opened)}
-        className="bg-blue-500 text-white p-4 rounded-full z-10"
-      >
-        <CiChat1 className="w-6 h-6" />
-      </button>
-    </div>
-  );
-};
+    return (
+        <div className="fixed bottom-0 right-0 m-4">
+            <div className={chatStyle}>
+                <Chat prompt={prompt} />
+            </div>
+            <button 
+                onClick={() => setOpened(!opened)} 
+                className="bg-blue-500 text-white p-4 rounded-full z-10"
+            >
+                <CiChat1 className="w-6 h-6"/>
+            </button>
+        </div>
+    )
+}
 
 export default ChatPopup;
